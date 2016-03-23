e2a7d6f62dafc8c0d1cfd18ac16b4b94a46eaa55
hamuPP <734877275@qq.com>
2016-03-24 01:30:21 +0800

    perfect the list on divine Page

--- public/js/divine.js
+++ public/js/divine.js
@@ -1,7 +1,7 @@
-/*根据后台返回的cardSum值，给第一个ul的li添加active样式*/
+/*根据后台返回的cardSum值，给第一个ul(张数的ul)的li添加active样式*/
 var cardSumUl = document.getElementById("cardSumUl"),
     cardSumLis = cardSumUl.getElementsByTagName("li"),
-    cardSum = cardSumUl.dataset.cardsum ||cardSumUl.dataset.cardSum,
+    cardSum = cardSumUl.dataset.cardsum,
     activeCardSumLi=null;
 switch (cardSum){
     case 'all':
@@ -27,5 +27,35 @@ switch (cardSum){
 };
 activeCardSumLi.className="active";
 
-/*根据选择张数不同，重新给类别指定递交地址*/
+/*根据后台返回的expertIn值，给第二个ul(擅长的ul)的li添加active样式*/
+var expertInUl = document.getElementById("expertInUl"),
+    expertInLis = expertInUl.getElementsByTagName("li"),
+    expertIn = expertInUl.dataset.expertin,
+    activeExpertInLi=null;
+switch (expertIn){
+    case 'all':
+        activeExpertInLi=expertInLis[0];
+        break;
+    case '通用':
+        activeExpertInLi=expertInLis[1];
+        break;
+    case '爱情':
+        activeExpertInLi=expertInLis[2];
+        break;
+    case '学业':
+        activeExpertInLi=expertInLis[3];
+        break;
+    case '友谊':
+        activeExpertInLi=expertInLis[4];
+        break;
+    case '事业':
+        activeExpertInLi=expertInLis[5];
+        break;
+    case '财运':
+        activeExpertInLi=expertInLis[6];
+        break;
+    default:
+        activeExpertInLi=expertInLis[0];
+};
+activeExpertInLi.className="active";
 
--- routes/index.js
+++ routes/index.js
@@ -22,23 +22,27 @@ exports.divineShowAll = function(req,res){
     var con = db.dbGetCon(),
         curpage = req.query.curpage,
         cardSum = req.query.cardSum || 'all',
-        expertIn = req.query.expertIn || '通用',
+        expertIn = req.query.expertIn || 'all',
         sql,
         condition=[];
-    if(cardSum == 'all'){
+    if(cardSum == 'all' && expertIn == 'all'){
+        sql = "select pxName,pxCardSum,pxExpertIn,pxSummary,pxBanner,pxDefaultPaizu from paixing";
+//        condition = [expertIn];
+    }else if(cardSum == 'all'){
         sql = "select pxName,pxCardSum,pxExpertIn,pxSummary,pxBanner,pxDefaultPaizu from paixing where pxExpertIn = ?";
         condition = [expertIn];
     }else{
-        sql = "select pxName,pxCardSum,pxExpertIn,pxSummary,pxBanner,pxDefaultPaizu from paixing where pxCardSum = ? and pxExpertIn = ?";
-        condition = [cardSum,expertIn];
+        sql = "select pxName,pxCardSum,pxExpertIn,pxSummary,pxBanner,pxDefaultPaizu from paixing where pxCardSum = ?";
+        condition = [cardSum];
     }
+
     db.queryByPage(con,curpage,8,sql,condition,function(e,r,f,page){
         if(e){
             console.log("有错误"+e);
         }else{
-
             page.r = r;
             page.cardSum = cardSum;
+            page.expertIn = expertIn;
             console.log("index.js 29LINE: "+JSON.stringify(page));
             res.render("divine",page);
         }
