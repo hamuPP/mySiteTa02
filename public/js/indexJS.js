cc027fac5875c604a056f5078da77239af1d7609
hamuPP <734877275@qq.com>
2016-03-28 15:01:42 +0800

    update

--- public/js/indexJS.js
+++ public/js/indexJS.js
@@ -2,6 +2,8 @@
  * Created by ty on 2016/2/1.
  */
 ;$(function(){
+    var bgImgWraper = document.getElementById("background-img-wrap-full");
+
     /**
     * 判断浏览器内核
     */
@@ -21,6 +23,9 @@
 //    document.oncontextmenu=function(){
 //        return false;
 //    };
+    /*设置背景图容器高度*/
+    var currentWidth = bgImgWraper.offsetWidth;
+    bgImgWraper.style.height=currentWidth*0.75+'px';
 });
 
 function checkUserAgent(){
