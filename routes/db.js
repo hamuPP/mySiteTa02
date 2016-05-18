03980033f650243570254363b92f24dc581df59c
hamuPP <734877275@qq.com>
2016-05-19 02:16:37 +0800

    change db port

--- routes/db.js
+++ routes/db.js
@@ -8,7 +8,7 @@ function dbGetCon(){
     var con = mysql.createConnection({
         host:"127.0.0.1",
         database:"d_tarotall",
-        port:3307,
+        port:3306,
         user:"root",
         password:"cc77"
     });
